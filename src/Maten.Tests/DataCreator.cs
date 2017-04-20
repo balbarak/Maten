be881c9862af857d9572123fb79a7162c78a67bd
Bader Albarrak <balbarak@live.com>
2017-04-20 03:36:00 +0300

    add stopwords and specialwords as json

--- src/Maten.Tests/DataCreator.cs
+++ src/Maten.Tests/DataCreator.cs
@@ -1,5 +1,9 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Maten.Tests.Repo;
+using System.Linq;
+using Newtonsoft.Json;
+using System.IO;
 
 namespace Maten.Tests
 {
@@ -9,7 +13,23 @@ namespace Maten.Tests
         [TestMethod]
         public void FillStopwords()
         {
+            string path = @"D:\Repos\Maten\src\Maten.Core\Data";
 
+            using (VertoDbEntities context = new VertoDbEntities())
+            {
+                var stopwords = context.Stopwords.ToList();
+                var specialwords = context.Specialwords.ToList();
+
+                var stopWordsJson = JsonConvert.SerializeObject(stopwords);
+
+                var specialWordsJson = JsonConvert.SerializeObject(specialwords);
+
+                File.WriteAllText($"{path}\\Stopwords.json", stopWordsJson);
+                File.WriteAllText($"{path}\\Specialwords.json", specialWordsJson);
+
+
+
+            }
         }
     }
 }
